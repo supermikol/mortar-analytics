a15c06a80fb4d5c36ad9c17c3c51231cec8c43a1
supermikol <mdu123@gmail.com>
2016-03-05 20:45:11 -0800

    rename the method for creating entry, and fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'history/index' => 'pages#history'
   get 'modal' => 'pages#modal'
   get 'transaction/add' => 'pages#form'
-  post 'transaction/add' => 'pages#add_entry'
+  post 'transaction/add' => 'pages#create_entry'
 
   get 'stacked_chart' => 'pages#stacked_chart'
 end
